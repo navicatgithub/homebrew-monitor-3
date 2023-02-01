1e2f9584430ca977f05661433ea3a99fc7dc3a12
wesley <wpl814@gmail.com>
2023-02-01 15:03:16 +0800

    Update navicatmonitor to v3.0.0.3

---
+++ README.md
@@ -0,0 +1,6 @@
+Navicat Monitor Homebrew Tap
+============================
+
+This is a [Homebrew][brew] tap for formulae for Navicat Monitor developed by Navicat.
+
+[brew]: http://brew.sh/
---
+++ navicatmonitor3.rb
@@ -0,0 +1,59 @@
+class Navicatmonitor3 < Formula
+  desc "Navicat Monitor is a safe, simple and agentless remote server monitoring tool that is packed with powerful features to make your monitoring effective as possible."
+  homepage "https://www.navicat.com/en/products#navicat-monitor"
+  url "https://navicat-download.oss-us-east-1.aliyuncs.com/monitor3-download/homebrew/navicat-monitor_3.0.0.3.tar.gz"
+  sha256 "1ad5ac25557c54d202a36da447be5f2bb057a4c53b9e71c7a45a322d6f5844a2"
+
+  bottle :unneeded
+
+  def install
+    # Preload
+    system "./install.sh"
+    libexec.install Dir["*"]
+
+    # Symlink var and tmp to persist across version update
+    rm_rf "#{libexec}/var"
+    mkdir_p "#{var}/navicatmonitor/var"
+    ln_s "#{var}/navicatmonitor/var", "#{libexec}/var"
+
+    rm_rf "#{libexec}/tmp"
+    mkdir_p "#{var}/navicatmonitor/tmp"
+    ln_s "#{var}/navicatmonitor/tmp", "#{libexec}/tmp"
+
+    # Create wrapper binary
+    bin.write_exec_script "#{libexec}/wrapper"
+    mv "#{bin}/wrapper", "#{bin}/navicatmonitor"
+  end
+
+  plist_options :startup => "true"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <dict>
+        <key>Crashed</key>
+        <true/>
+      </dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{libexec}/wrapper</string>
+        <string>start</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>WorkingDirectory</key>
+      <string>#{libexec}</string>
+    </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    system "#{bin}/navicatmonitor version"
+  end
+end
