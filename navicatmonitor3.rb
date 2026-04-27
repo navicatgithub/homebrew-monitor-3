0d8bd5c2a837b2f239442d5caa3511eca0b0fcf1
Navicat <github@navicat.com>
2026-04-27 10:50:21 +0800

    Update navicatmonitor to v3.1.5.3

--- navicatmonitor3.rb
+++ navicatmonitor3.rb
@@ -1,8 +1,8 @@
 class Navicatmonitor3 < Formula
   desc "Navicat Monitor is a safe, simple and agentless remote server monitoring tool that is packed with powerful features to make your monitoring effective as possible."
   homepage "https://www.navicat.com/en/products#navicat-monitor"
-  url "https://navicat-download.oss-us-east-1.aliyuncs.com/monitor3-download/homebrew/navicat-monitor_3.1.4.3.tar.gz"
-  sha256 "5df714be77a8365abd65e3a1b0682588189fb89bf708ba1a5dab32b40cee879e"
+  url "https://navicat-download.oss-us-east-1.aliyuncs.com/monitor3-download/homebrew/navicat-monitor_3.1.5.3.tar.gz"
+  sha256 "51705819171b99ca11bb2274d88e0b5b79fad1cb24e57ef16a2601c07b798635"
 
   def install
     # Preload
